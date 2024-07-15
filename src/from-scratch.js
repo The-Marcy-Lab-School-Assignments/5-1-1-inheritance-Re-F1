949d35d185156ac67865d88c66e29db7e33b70bf
Fiona Baci <fionabaci04@gmail.com>
2024-07-15 09:59:54 -0400

    almost

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,42 @@
 class Quadrilateral {
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+    this.side4 = side4
+  }
 
+  getPerimeter(){
+    return this.side1 + this.side2 + this.side3 + this.side4
+  }
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+constructor(side1, side2) {
+  super(side1, side2, side1, side2)
 }
 
-class Square {
+getArea(){
+  return this.side1*this.side2
+}
+}
 
+class Square extends Quadrilateral{
+ constructor(side){
+  super(side, side, side, side)
+ }
+
+ getPerimeter() {
+  return 4 * this.side1;
+}
+
+getArea() {
+  return this.side1 * this.side1;
+}
+
+getDiagonal() {
+  return Math.sqrt(2) * this.side1;
+}
 }
 
 /* Be creative with this one! */
